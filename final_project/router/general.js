bdba807a088e3e64fa309a8a90171156d5827ecd
MANOJKUMAR-CA <97181189+MANOJKUMAR-CA@users.noreply.github.com>
2024-03-30 15:40:48 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,245 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.query.username;
+  const password = req.query.password;
+
+  if (username && password) {
+    if (isValid(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise resolved")
+    },6000)})
+    
+    myPromise.then((successMessage) => {
+        console.log("callback " + successMessage)
+        res.send(JSON.stringify({books},null, 4));
+    })
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise resolved")
+    },6000)})
+    
+    myPromise.then((successMessage) => {
+        console.log("callback " + successMessage)
+        let isbn = req.params.isbn;
+        res.send(books[isbn]);
+
+    })
+
+    console.log("After calling promise");
+  // return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise resolved ")
+    },6000)})
+    
+
+    myPromise.then((successMessage) => {
+        console.log("callback " + successMessage)
+        let author = req.params.author;
+        for(var i in books)
+        {
+                let details = books[i];
+                if(details)
+                {
+                    if(author === details["author"])
+                    {
+                        res.send(books[i]);
+                    }
+                }
+        }
+
+    })
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise resolved in getting book details based on title")
+    },6000)})
+    
+    //Console log before calling the promise
+    console.log("Before calling promise");
+    //Call the promise and wait for it to be resolved and then print a message.
+    myPromise.then((successMessage) => {
+        console.log("From Callback " + successMessage)
+        let title = req.params.title;
+        for(var i in books)
+        {
+              let details = books[i];
+              if(details)
+              {
+                  if(title === details["title"])
+                  {
+                      res.send(books[i]);
+                  }
+              }
+        }
+    })
+
+    //Console log after calling the promise
+    console.log("After calling promise");
+
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let review = books[isbn]["reviews"];
+  res.send(JSON.stringify({review}, null,4));
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
+
+
+async function getBookListAsync(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw error; // Re-throw the error for handling in the route
+  }
+}
+
+//Task-10 With Promise
+public_users.get('/promise', function (req, res) {
+  try {
+    getBookListWithPromise('http://localhost:5000/') 
+      .then(bookList => {
+        res.json(bookList);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving book list" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unexpected error" });
+  }
+});
+
+//Task-10 With Async
+public_users.get('/async', async function (req, res) {
+  try {
+    const bookList = await getBookListAsync('http://localhost:5000/'); //
+    res.json(bookList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book list" });
+  }
+});
+
+// Task-11 With Promise
+public_users.get('/promise/isbn/:isbn', function (req, res) {
+  try {
+    const requestedIsbn = req.params.isbn;
+    getBookListWithPromise("http://localhost:5000/isbn/" + requestedIsbn) 
+      .then(book => {
+        res.json(book);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving book details" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unexpected error" });
+  }
+});
+// Task-11 With Async
+public_users.get('/async/isbn/:isbn', async function (req, res) {
+  try {
+    const requestedIsbn = req.params.isbn;
+    const book = await getBookListAsync("http://localhost:5000/isbn/" + requestedIsbn);
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book details" });
+  }
+});
+
+// Task-12 With Promise
+public_users.get('/promise/author/:author', function (req, res) {
+  try {
+    const requestedAuthor = req.params.author;
+    getBookListWithPromise("http://localhost:5000/author/" + requestedAuthor) 
+      .then(book => {
+        res.json(book);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving book details" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unexpected error" });
+  }
+});
+// Task-12 With Async
+public_users.get('/async/author/:author', async function (req, res) {
+  try {
+    const requestedAuthor = req.params.author;
+    const book = await getBookListAsync("http://localhost:5000/author/" + requestedAuthor);
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book details" });
+  }
+});
+
+// Task-13 With Promise
+public_users.get('/promise/title/:title', function (req, res) {
+  try {
+    const requestedTitle = req.params.title;
+    getBookListWithPromise("http://localhost:5000/title/" + requestedTitle) 
+      .then(book => {
+        res.json(book);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving book details" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unexpected error" });
+  }
+});
+// Task-13 With Async
+public_users.get('/async/title/:title', async function (req, res) {
+  try {
+    const requestedTitle = req.params.title;
+    const book = await getBookListAsync("http://localhost:5000/title/" + requestedTitle);
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book details" });
+  }
+});
+
 
 module.exports.general = public_users;
